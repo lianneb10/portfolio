ebba40179a5e997d9371766ebae39ea6e58cd254
lbaratea <aratealianne@gmail.com>
2022-11-04 13:59:10 -0700

    made heart envelope close and open on index page

--- app.js
+++ app.js
@@ -17,3 +17,23 @@ colorBar.addEventListener("mouseleave", reAnimate)
 function reAnimate (event) {
     colorBar.classList.add("animate");
 }
+
+//when in mobile version, clicking in the menu icon will open and close the navigation bar
+
+const menuIcon = document.querySelector(".menuIcon")
+
+menuIcon.addEventListener("click", menuOpen)
+
+function menuOpen(event) {
+    document.querySelector(".navbar").classList.toggle("hidden");
+}
+
+//when user clicks envelope icon on bottom right of screen, they receive a randomized message
+
+const openPls = document.querySelector(".envelope")
+
+openPls.addEventListener("click", giveMessage)
+
+function giveMessage(event) {
+    document.querySelector(".envelope").classList.toggle("heart")
+}
